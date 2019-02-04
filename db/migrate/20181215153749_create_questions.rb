c801191e5bdd0c8158d12713f34a18b53e2d5506
Juan Manuel <juanmaotto9@gmail.com>
2019-02-04 15:45:52 +0000

    cambiadas restricciones

--- db/migrate/20181215153749_create_questions.rb
+++ db/migrate/20181215153749_create_questions.rb
@@ -4,8 +4,8 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :description
       t.boolean :status
-      t.references :user, foreign_key: true
-      t.references :answer, foreign_key: true
+      t.references :user#, foreign_key: true
+      t.references :answer#, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20181215154019_create_answers.rb
+++ db/migrate/20181215154019_create_answers.rb
@@ -1,9 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.references :question, foreign_key: true
+      t.references :question#, foreign_key: true
       t.string :content
-      t.references :user, foreign_key: true
+      t.references :user#, foreign_key: true
       t.boolean :correct_answer
 
       t.timestamps
